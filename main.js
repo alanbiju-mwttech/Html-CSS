c11033efc83d0bcfa6351a1390b595e0d53dc706
Alan Biju <alan.biju@mwttech.com>
2025-08-01 16:56:59 +0530

    Till Top Offers

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const sidebar = document.getElementById("sidebar");
+let products = {}
 
 function myFunction(x) {
        x.classList.toggle("change");
        sidebar.classList.toggle("active");
 }
 
+document.querySelectorAll('.card').forEach(card => {
+       card.addEventListener('click', function () {
+              const image = this.querySelector('img').src;
+              const name = this.querySelector('.text-head').innerText;
+              const price = this.querySelector('.text-body').innerText;
+
+              products = { image, name, price };
+              sessionStorage.setItem('selectedProduct', JSON.stringify(product));
+              window.location.href = './products.html';
+       });
+});
+
+const product = JSON.parse(sessionStorage.getItem('selectedProduct'))
+
+if (product) {
+       document.getElementById('product-details').innerHTML = `
+      <img src="${product.image}" alt="${product.name}" width="300">
+      <h2>${product.name}</h2>
+      <p style="font-size: 1.1rem, color: red !important">Price: ${product.price}</p>
+    `;
+} else {
+       document.getElementById('product-details').innerHTML = `<p>No product found.</p>`;
+}
